55f7dd98754092d10679eb1faa274b0233973a84
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-03 17:21:51 -0700

    Adds migrations for reviews, faker gem, review model tests and file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@ Item.destroy_all
 User.destroy_all
 
 admin = create(:admin)
-user = create(:user)
+user_1 = create(:user)
+user_2 = create(:user)
+user_3 = create(:user)
+user_4 = create(:user)
 merchant_1 = create(:merchant)
 
 merchant_2, merchant_3, merchant_4 = create_list(:merchant, 3)
@@ -28,26 +31,27 @@ inactive_item_2 = create(:inactive_item, user: inactive_merchant_1)
 Random.new_seed
 rng = Random.new
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_1)
 create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: rng.rand(3).days.ago, updated_at: rng.rand(59).minutes.ago)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 create(:fulfilled_order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: rng.rand(5).days.ago, updated_at: rng.rand(59).minutes.ago)
 create(:fulfilled_order_item, order: order, item: item_4, price: 4, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 
-order = create(:order, user: user)
+order = create(:order, user: user_2)
 create(:order_item, order: order, item: item_1, price: 1, quantity: 1)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).days.ago, updated_at: rng.rand(23).hours.ago)
 
-order = create(:cancelled_order, user: user)
+order = create(:cancelled_order, user: user_3)
 create(:order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 create(:order_item, order: order, item: item_3, price: 3, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 
-order = create(:completed_order, user: user)
+order = create(:completed_order, user: user_4)
 create(:fulfilled_order_item, order: order, item: item_1, price: 1, quantity: 1, created_at: rng.rand(4).days.ago, updated_at: rng.rand(59).minutes.ago)
 create(:fulfilled_order_item, order: order, item: item_2, price: 2, quantity: 1, created_at: rng.rand(23).hour.ago, updated_at: rng.rand(59).minutes.ago)
 
-Review.create(title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
-Review.create(title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
-Review.create(title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
-Review.create(title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
-Review.create(title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_1, user: user_1, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_2, user: user_2, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_3, user: user_3, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_4, user: user_4, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_1, user: user_3, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
+Review.create(item: item_4, user: user_3, title: Faker::LeagueOfLegends.quote, description: Faker::MichaelScott.quote, rating: Faker::Number.between(1, 5))
