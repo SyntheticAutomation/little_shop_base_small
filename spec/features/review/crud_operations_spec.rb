18f356c6d4775866135b0f4d2fdb289145ab4187
SyntheticAutomation <mclampet@msudenver.edu>
2019-01-05 13:33:41 -0700

    Adds an enable/disable test plus two links

--- spec/features/review/crud_operations_spec.rb
+++ spec/features/review/crud_operations_spec.rb
@@ -105,9 +105,21 @@ describe 'A registered user who visits our web app' do
     @review_1 = Review.create(title: "Amazing product!", rating: 5, description: "fgnribtdufbgiu feowapghi goeiwgbhigl932409htgibn", item: @item_1, user: @user)
     @review_2 = Review.create(title: "Garbage...", rating: 1, description: "my review goes here blah blah", item: @item_2, user: @user)
 
-    visit profile_reviews_path
+    visit profile_path
+
+    click_on "My Reviews"
+    expect(current_path).to eq(profile_reviews_path)
+
     expect(page).to have_content(@review_1.title)
     expect(page).to have_content(@review_2.description)
   end
+  it 'can disable/enable a review' do
+    @review_1 = Review.create(title: "Amazing product!", rating: 5, description: "fgnribtdufbgiu feowapghi goeiwgbhigl932409htgibn", item: @item_1, user: @user)
+    @review_2 = Review.create(title: "Garbage...", rating: 1, description: "my review goes here blah blah", item: @item_2, user: @user)
 
+    visit profile_reviews_path
+    click_link @review_1.title
+    expect(current_path).to eq(item_review_path(@item_1, @review_1))
+
+  end
 end
